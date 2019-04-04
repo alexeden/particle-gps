217f1d9015ff99fe3d31fdb2311be05413b88eb5
Alex Eden <alexandereden91@gmail.com>
2019-04-03 23:58:08 -0500

    Output GPS data to the main serial port

--- src/particle-gps.cpp
+++ src/particle-gps.cpp
@@ -1,12 +1,18 @@
 #include "Particle.h"
+#define GPSSerial Serial1
 
 void setup();
 void loop();
 
 void setup() {
+	Serial.begin(115200);
 
+	GPSSerial.begin(9600);
 }
 
 void loop() {
-
+	if (GPSSerial.available()) {
+		char c = GPSSerial.read();
+		Serial.write(c);
+	}
 }
