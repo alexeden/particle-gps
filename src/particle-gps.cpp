bae1f2e2161a48469720ad3c7c2772e098ab7dcf
Alex Eden <alexandereden91@gmail.com>
2019-04-19 00:22:20 -0500

    Log cell signal strength with GPS data

--- src/particle-gps.cpp
+++ src/particle-gps.cpp
@@ -36,13 +36,17 @@ void loop() {
 	if (timer > millis()) timer = millis();
 
 	// approximately every 2 seconds or so, print out the current stats
-	if (millis() - timer > 2000) {
-		timer = millis(); // reset the timer
+	if (millis() - timer > 5000) {
+		timer			   = millis(); // reset the timer
+		CellularSignal sig = Cellular.RSSI();
+		Serial << "Cell signal strength: " << sig.rssi << endl;
+		Serial << "Cell signal quality: " << sig.qual << endl;
+		Serial << "Cell access protocol: " << sig.getAccessTechnology() << endl;
+		Serial << "Cell private IP: " << Cellular.localIP() << endl;
 		Serial << "GPS fix: " << GPS.fix << endl;
 		Serial << "GPS fix quality: " << GPS.fixquality << endl;
 		Serial << "GPS satellites: " << GPS.satellites << endl;
-		Serial << endl
-			   << "Time: " << GPS.hour << ":" << GPS.minute << ":" << GPS.seconds << " " << GPS.milliseconds << endl;
+		Serial << "Time: " << GPS.hour << ":" << GPS.minute << ":" << GPS.seconds << " " << GPS.milliseconds << endl;
 		Serial << "Date: " << GPS.month << "/" << GPS.day << "/20" << GPS.year << endl;
 		if (GPS.fix) {
 			Serial.print("Location: ");
